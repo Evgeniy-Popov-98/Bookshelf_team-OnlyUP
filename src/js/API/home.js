152ae33031fa529150d200c46886893df5a78be8
YuliaKravchukS <ljuljek554@gmail.com>
2024-02-22 16:08:24 +0200

    error data.books

--- src/js/API/home.js
+++ src/js/API/home.js
@@ -1,14 +1,21 @@
 import {getBooks} from './api-books'
 import refs from './refs'
 
+export async function render(){
+    const data = await getBooks('top-books');
+    console.log(data.books);
+    renderBooks(data.books);
+}
+
 
 
 
 function bookTemplate(book) {
     const {book_image, author, title, id, contributor} = book;
+    console.log(book);
     return `<li class="list-category-books">
     <li class="card book-item" data-id="${id}">
-    <img class="book-img" src="${book_image}" alt="${contributor} ${tittle}">
+    <img class="book-img" src="${book_image}" alt="${contributor} ${title}">
     <h3>${title}</h3>
     <p>${author}</p>
 </li>
@@ -20,6 +27,7 @@ function booksTemplate(books) {
 }
 
 export function renderBooks(books) {
-    const markup = booksTemplate
-    refs.galleryBooks.innerHTML=markup;
+    const markup = booksTemplate(books)
+    refs.galleryBooks.insertAdjacentHTML('beforeend', markup);
 }
+
--- src/main.js
+++ src/main.js
@@ -1,9 +1,3 @@
-import { getBooks } from './js/API/api-books';
-import {renderBooks} from './js/API/home';
+import {render} from './js/API/home'
 
-
-document.addEventListener('DOMContentLoaded', async function(){
-    const data = await getBooks('top-books');
-    console.log(data);
-    // renderBooks(data.books);
-})
+render()
