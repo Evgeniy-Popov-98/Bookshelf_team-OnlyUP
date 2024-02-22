369789f789bfbdf2f0fea66fe24f1074a822533c
YuliaKravchukS <ljuljek554@gmail.com>
2024-02-22 23:47:32 +0200

    add home gallery

--- src/js/API/home.js
+++ src/js/API/home.js
@@ -1,33 +1,47 @@
+
 import {getBooks} from './api-books'
 import refs from './refs'
 
 export async function render(){
-    const data = await getBooks('top-books');
-    console.log(data.books);
-    renderBooks(data.books);
+    try {
+        const data = await getBooks('top-books');
+        for (const el of data) {
+            categoriesTemplate(el);           
+                renderBooks(el);
+        }
+    } catch (error) {
+        console.error('Failed to render books:', error);
+    }
 }
 
 
+function categoriesTemplate(categories){
+    const markup = `<li id="${categories.list_name}" class="list-category-books">
+    <h2 class="list-category-title">${categories.list_name}</h2>
+    <ul class="list-book"></ul>
+    <button type="button" class="btn-more">See more</button>
+  </li>`
+    refs.galleryBooks.insertAdjacentHTML('beforeend', markup);
+}
 
 
 function bookTemplate(book) {
-    const {book_image, author, title, id, contributor} = book;
-    console.log(book);
-    return `<li class="list-category-books">
-    <li class="card book-item" data-id="${id}">
+    const {book_image, author, title, _id, contributor, list_name} = book;
+    if(list_name)
+    return `
+    <li class="card book-item" data-id="${_id}">
     <img class="book-img" src="${book_image}" alt="${contributor} ${title}">
-    <h3>${title}</h3>
-    <p>${author}</p>
-</li>
-  <button>See more</button>
+    <h3 class="title-book">${title}</h3>
+    <p class="author">${author}</p>
 </li>`;
 }
 function booksTemplate(books) {
     return  books.map(bookTemplate).join('');
 }
 
-export function renderBooks(books) {
-    const markup = booksTemplate(books)
-    refs.galleryBooks.insertAdjacentHTML('beforeend', markup);
+export function renderBooks(el) {
+    const categoriesID = el.list_name;
+    const listBook = document.getElementById(categoriesID).querySelector('.list-book');
+    const markup = booksTemplate(el.books);
+    listBook.insertAdjacentHTML('beforeend', markup);
 }
-
