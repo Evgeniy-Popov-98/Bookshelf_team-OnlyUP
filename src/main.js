1bb4d82143bb25c09322682caf816b2dbfbcd87f
Evgeniy-Popov-98 <143257644+Evgeniy-Popov-98@users.noreply.github.com>
2024-02-22 13:04:44 +0200

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,13 +1,10 @@
 import { getBooks } from './js/API/api-books';
 import { renderBooks } from './js/rendering-book';
 
-// renderBooks();
-
 async function main() {
   let endpoints = 'category';
   let category = 'Audio Fiction';
   const newCategory = await getBooks(endpoints, category);
-  console.log(newCategory);
   renderBooks(newCategory);
 }
 
