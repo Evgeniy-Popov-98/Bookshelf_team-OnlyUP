26ef6d9d284a8ee5e43b511ac061a94c607c876a
YuliaKravchukS <ljuljek554@gmail.com>
2024-02-23 23:27:52 +0200

    add scroll

--- src/js/API/refs.js
+++ src/js/API/refs.js
@@ -3,5 +3,6 @@ export default
 {
     galleryBooks:document.querySelector('.js-gallery-books'),
     btnMore:document.querySelector('.btn-more'),
-        listCategoryTitle:document.querySelector('.list-category-title')
+    listCategoryTitle:document.querySelector('.list-category-title'),
+    scrollTop:document.getElementById('scrollTop'),
 }
--- src/main.js
+++ src/main.js
@@ -2,5 +2,7 @@
 import { getBooks } from './js/API/api-books';
 import './js/switch-button';
 import {render} from './js/API/home'
+import './js/scroll-ToUP'
+
 
 render()
