dba07a2851c27125a3ab0c7ffb2ae39481f4814f
PYLYP CHELBARAKH <143128807+mrignis@users.noreply.github.com>
2024-02-24 04:39:58 -0500

    up

---
+++ src/js/loader.js
@@ -0,0 +1,20 @@
+.loader-container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+}
+
+.loader {
+  border: 8px solid rgba(0, 0, 0, 0.1); /* Півпрозорі лінії loader */
+  border-top: 8px solid #3498db; /* Колір верхньої лінії */
+  border-radius: 50%;
+  width: 50px;
+  height: 50px;
+  animation: spin 1s linear infinite; /* Анімація обертання */
+}
+
+@keyframes spin {
+  0% { transform: rotate(0deg); }
+  100% { transform: rotate(360deg); }
+}
