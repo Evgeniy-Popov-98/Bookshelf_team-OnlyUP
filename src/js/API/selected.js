390f15973c5e05bd0d446ee02d3f80ead5334dd6
AnatoliyMW <143413773+AnatoliyMW@users.noreply.github.com>
2024-02-24 10:59:06 +0100

    selected

--- src/js/API/selected.js
+++ src/js/API/selected.js
@@ -1,51 +1,27 @@
 import { getBooks } from './api-books';
-import refs from './refs';
-import { errorMessage } from './messageError';
 
-export async function render(category) {
+export async function homeCategory(END_POINT, categoriesBooks) {
   try {
-    const data = await getBooks(category);
-    categoriesTemplate({ list_name: category });
-    if (data.books.length >= 1) {
-      renderBooks(data);
-    } else {
-      errorMessage(`Sorry, there are no items in the "${category}" category`);
-    }
+    const data = await getBooks(END_POINT, categoriesBooks);
+    renderBooks(data);
   } catch (error) {
-    errorMessage(`Failed to render books: ${error}`);
+    console.error('Failed to fetch books:', error);
   }
 }
 
-function categoriesTemplate(categories) {
-  const markup = `<li id="${categories.list_name}" class="list-category-books">
-    <h2 class="list-category-title">${categories.list_name}</h2>
-    <ul class="list-book">
-     </ul>    
-    </li>
-    <button type="button" class="btn-more">See more</button>`;
-  refs.galleryBooks.insertAdjacentHTML('beforeend', markup);
-}
-
-function bookTemplate(book) {
-  const { book_image, author, title, _id, contributor, list_name } = book;
-  if (list_name)
-    return `
-    <li class="card book-item" data-id="${_id}">
-    <img class="book-img" src="${book_image}" alt="${contributor} ${title}">
-    <h3 class="title-book">${title}</h3>
-    <p class="author">${author}</p>
-    </li>
-   `;
-}
-function booksTemplate(books) {
-  return books.map(bookTemplate).join('');
-}
-
-export function renderBooks(el) {
-  const categoriesID = el.list_name;
-  const listBook = document
-    .getElementById(categoriesID)
-    .querySelector('.list-book');
-  const markup = booksTemplate(el.books);
-  listBook.insertAdjacentHTML('afterbegin', markup);
+export function renderBooks(books) {
+  const gallery = document.querySelector('.gallery-books');
+  const markup = books
+    .map(book => {
+      return `
+      <li class="gallery-book-item">
+        <div class="container-item">
+          <img class="gallery-image" src="${book.book_image}" alt="${book.description}">
+          <h3 class="title-book">${book.title}</h3>
+          <p class="author-book">${book.author}</p>
+        </div>
+      </li>`;
+    })
+    .join('');
+  gallery.insertAdjacentHTML('beforeend', markup);
 }
--- src/main.js
+++ src/main.js
@@ -1,3 +1,7 @@
-import {render} from './js/API/home'
+import { render } from './js/API/home';
+import { homeCategory } from './js/API/selected';
+const END_POINT = 'category';
+const categoriesBooks = 'Handcover Fiction';
+homeCategory(END_POINT, categoriesBooks);
 
-render()
+render();
