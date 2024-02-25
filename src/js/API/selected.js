4545db146238b26137684692533a2174a55118c4
Evgeniy-Popov-98 <evgeniy6183@gmail.com>
2024-02-25 22:24:40 +0200

    Update selected.js

--- src/js/API/selected.js
+++ src/js/API/selected.js
@@ -1,10 +1,22 @@
 import { getBooks } from './api-books';
+// import { GetBook } from './modal';
 
 export async function homeCategory(END_POINT, categoriesBooks) {
   try {
     const data = await getBooks(END_POINT, categoriesBooks);
     renderBooks(data);
     updateTitle(categoriesBooks);
+
+    const openModal = document.querySelectorAll('.booksgallery-item');
+    document.addEventListener('click', event => {
+      for (const item of openModal) {
+        let data = item.dataset.id;
+        if (event.target.parentNode === item) {
+          console.log(data);
+          //   GetBook(data);
+        }
+      }
+    });
   } catch (error) {
     console.error('Failed to fetch books:', error);
   }
@@ -15,12 +27,10 @@ function renderBooks(books) {
   const markup = books
     .map(book => {
       return `
-      <li class="booksgallery-item" id="book-${book._id}">
-        <div class="container-item">
+      <li class="booksgallery-item" data-id="${book._id}">
           <img class="gallery-image" src="${book.book_image}" alt="${book.description}">
           <h3 class="name-book">${book.title}</h3>
           <p class="author-book">${book.author}</p>
-        </div>
       </li>`;
     })
     .join('');
