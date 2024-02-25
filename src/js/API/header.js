6336994b91e7661b2ebe6dc9302f4b5f349755d6
vika59216 <viktoriiadupreez@gmail.com>
2024-02-25 20:17:35 +0100

    header1

---
+++ src/js/API/header.js
@@ -0,0 +1,16 @@
+/*document.querySelector("#submit").onclick = function () {
+    console.log();
+}
+
+
+
+function send1() {
+    console.log();
+}
+
+function send2() {
+    console.log();
+}*/
+
+function aler
+
