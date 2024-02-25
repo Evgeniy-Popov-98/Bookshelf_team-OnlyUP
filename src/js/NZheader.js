9c363b885027e631890eb3ffc28c8e0be6f65b5c
NataliaZgurska <143435353+NataliaZgurska@users.noreply.github.com>
2024-02-25 23:06:09 +0200

    header user sign up btn

--- src/js/NZheader.js
+++ src/js/NZheader.js
@@ -1,8 +1,5 @@
 const homeBtn = document.querySelector('.js-hhome');
 const shoppingBtn = document.querySelector('.js-hshopping');
-
-console.log(homeBtn);
-console.log(shoppingBtn);
  
 homeBtn.addEventListener('click', onHomeBtnClick);
 shoppingBtn.addEventListener('click', onShoppingBtnClick);
