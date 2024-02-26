ab1869b9fc528c9d1eba5617fc5f556e14c47315
PYLYP CHELBARAKH <143128807+mrignis@users.noreply.github.com>
2024-02-26 04:11:26 -0500

    uo

--- src/main.js
+++ src/main.js
@@ -1,3 +1,5 @@
 import { getBooks } from './js/API/api-books';
 import { getBooks } from './js/add-removebook';
 import { getBooks } from './js/pagination';
+import { getBooks } from './js/add-card';
+import { getBooks } from './js/remove-card';
