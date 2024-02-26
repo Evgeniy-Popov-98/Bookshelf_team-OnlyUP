11f25f51e35656354584881ffc66a23a9fbb952a
Evgeniy-Popov-98 <evgeniy6183@gmail.com>
2024-02-26 21:17:23 +0200

    fix

--- src/js/API/all-categories.js
+++ src/js/API/all-categories.js
@@ -1,60 +1,37 @@
 import axios from 'axios';
-import {getBooks} from './api-books';
+import { getBooks } from './api-books';
 import refs from './refs';
-import { errorMessage } from './messageError';
-import { render, renderBooks } from './home';
+// import { render } from './js/API/home';
 
 export async function renderCategoriesList() {
-  try {
-    const data = await getBooks('category-list');
-    let markup = `<li data-category="top-books" class="category-books-item">
-            <a href="https://books-backend.p.goit.global/books/top-books" class="category-link"
-            data-category="">All categories</a></li>`;
-      
-    data.forEach(category => {
-      markup += `<li class="category-books-item" data-category="${category.list_name}">
-                <a href="https://books-backend.p.goit.global/books/category?category=${encodeURIComponent(category.list_name)}" 
-                class="category-link">${category.list_name}</a>
-            </li>`;
-    });
+  const data = await getBooks('category-list');
+  let markup = `<li data-category="top-books" class="category-books-item">All categories</li>`;
 
-    refs.allListCategories.insertAdjacentHTML('beforeend', markup);
+  data.forEach(category => {
+    markup += `<li class="category-books-item" data-category="${category.list_name}">${category.list_name}</li>`;
+  });
+  refs.allListCategories.insertAdjacentHTML('beforeend', markup);
 
-    const categoryLinks = document.querySelectorAll('.category-books-item');
+  const categoryLinks = document.querySelectorAll('.category-books-item');
 
-    categoryLinks.forEach(link => {
-      link.addEventListener('click', handleCategoryClick);
-  })
-  
-  } catch (error) {
-    errorMessage(`Failed to render books: ${error}`);
+  categoryLinks.forEach(link => {
+    link.addEventListener('click', handleCategoryClick);
+  });
+
+  function handleCategoryClick(event) {
+    event.preventDefault();
+    const category = event.target.innerHTML;
+    for (const item of data) {
+      if (item.list_name === category) {
+        // renderBooksByCategory(category);
+      }
+    }
+    if (category === 'All categories') {
+      const topCategory = document.querySelector('.category-books-item');
+      console.log(topCategory);
+      const topBooks = topCategory.dataset.category;
+      console.log(topBooks);
+      //   render(topBooks);
+    }
   }
 }
-
-async function handleCategoryClick(event) {
-  event.preventDefault();
-  const category = event.target.innerHTML;
-
-  if (category) {
-    await renderBooksByCategory(category);
-  }
-}
-
-async function renderBooksByCategory(category) {
-  
-  try {
-    const data = await getBooks('category', category);
-    renderBooks(data);
-  } catch (error) {
-    errorMessage(`Failed to render books: ${error}`);
-  }
-}
-
-
-  
-
-
-
-
-
-
--- src/main.js
+++ src/main.js
@@ -1,8 +1,7 @@
-
 import { getBooks } from './js/API/api-books';
-import './js/switch-button';
-import { render } from './js/API/home'
-import { renderCategoriesList } from './js/API/all-categories'
+// import './js/switch-button';
+import { render } from './js/API/home';
+import { renderCategoriesList } from './js/API/all-categories';
 
 render();
 renderCategoriesList();
