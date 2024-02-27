feec02557d91cfa2e5b973e714d5b2cd97f9c65d
AnettX <ann.badova@gmail.com>
2024-02-28 00:21:31 +0200

    active-page style
    added styles for the active page and functions for changing them.
    In progress: to make styles fix smoothly when changing the active page.

--- src/js/NZheader.js
+++ src/js/NZheader.js
@@ -1,21 +1,15 @@
 import { addToShoppingList } from './API/storage';
 
-const homeBtn = document.querySelector('.js-hhome');
-const shoppingBtn = document.querySelector('.js-hshopping');
+document.addEventListener('DOMContentLoaded', function() {
+  const currentLocation = window.location.href;
+  const homeBtn = document.querySelector('.js-hhome');
+  const shoppingBtn = document.querySelector('.js-hshopping');
 
-document.addEventListener('DOMContentLoaded', onHomeBtnClick);
 
-homeBtn.addEventListener('click', onHomeBtnClick);
-shoppingBtn.addEventListener('click', onShoppingBtnClick);
+  if (currentLocation.includes('index.html')) {
+    homeBtn.classList.add('active-page');
+  } else if (currentLocation.includes('shoppinglist.html')) {
+    shoppingBtn.classList.add('active-page');
+  }
+});
 
-function onHomeBtnClick() {
-  homeBtn.classList.add('hhome-btn');
-  shoppingBtn.classList.remove('hshopping-btn');
-}
-
-function onShoppingBtnClick() {
-  homeBtn.classList.remove('hhome-btn');
-  shoppingBtn.classList.add('hshopping-btn');
-}
-
-shoppingBtn.addEventListener('click', addToShoppingList);
