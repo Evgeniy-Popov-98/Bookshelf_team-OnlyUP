2b3703266689737c553c31efe8c6565b095137f0
PYLYP CHELBARAKH <143128807+mrignis@users.noreply.github.com>
2024-02-27 00:59:24 -0500

    Update shoppinglist.js

--- src/shoppinglist.js
+++ src/shoppinglist.js
@@ -1,3 +1,10 @@
 import './js/switch-button';
 import './js/NZheader';
 import './js/API/storage';
+// shoppinglist.js
+
+import { addToShoppingList } from './js/API/storage';
+
+document.addEventListener('DOMContentLoaded', async function () {
+  await addToShoppingList();
+});
