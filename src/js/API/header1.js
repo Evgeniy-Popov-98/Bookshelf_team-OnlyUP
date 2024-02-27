91eb3eefce99e8d79e698ea6eb38cf6d0ee8791d
vika59216 <viktoriiadupreez@gmail.com>
2024-02-27 18:17:01 +0100

    header1

--- src/js/API/header1.js
+++ src/js/API/header1.js
@@ -1,23 +1,30 @@
-let isVisible = true;
-let isVisible = false; 
 
-button.addEventListener('click', function() {
-  const hiddenItems = document.querySelectorAll('.list-item:nth-last-child(-n+3)'); 
-  const visibleItems = document.querySelectorAll('.list-item:nth-child(n+4):nth-child(-n+9)'); 
-  if (!isVisible) {
-    hiddenItems.forEach(item => {
-      item.classList.remove('hidden'); 
-    });
-    visibleItems.forEach(item => {
-      item.classList.add('hidden'); 
-    });
-  } else {
-    hiddenItems.forEach(item => {
-      item.classList.add('hidden'); 
-    });
-    visibleItems.forEach(item => {
-      item.classList.remove('hidden'); 
-    });
-  }
-  isVisible = !isVisible; 
+let isVisible = false; 
+let firstButton = document.querySelector('.plus-btn');
+let secondButton = document.querySelector('.second-btn');
+
+firstButton.addEventListener('click', function () {
+  const hiddenItems = document.querySelectorAll('.header-menu:nth-child(n+4):nth-child(-n+9)'); 
+  const visibleItems = document.querySelectorAll('.header-menu:nth-last-child(-n+3)'); 
+  hiddenItems.forEach(item => {
+    item.classList.remove('hidden'); 
+  });
+  visibleItems.forEach(item => {
+    item.classList.add('hidden'); 
+  });
+  firstButton.classList.add('hidden');
+  secondButton.classList.remove('hidden');
+});
+
+secondButton.addEventListener('click', function () {
+  const hiddenItems = document.querySelectorAll('.header-menu:nth-child(n+7):nth-child(-n+9)'); 
+  const visibleItems = document.querySelectorAll('.header-menu:nth-child(-n+6)'); 
+  hiddenItems.forEach(item => {
+    item.classList.remove('hidden'); 
+  });
+  visibleItems.forEach(item => {
+    item.classList.add('hidden'); 
+  });
+  secondButton.classList.add('hidden');
+  firstButton.classList.remove('hidden');
 });
