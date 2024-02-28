f728db9790943a7e855c9274d33eaff44be6d55f
PYLYP CHELBARAKH <143128807+mrignis@users.noreply.github.com>
2024-02-27 20:41:40 -0500

    up

--- src/shoppinglist.js
+++ src/shoppinglist.js
@@ -4,9 +4,3 @@ import './js/API/storage';
 import './js/localStorage';
 import './js/pagination';
 // shoppinglist.js
-
-import { addToShoppingList } from '/js/localStorage';
-
-document.addEventListener('DOMContentLoaded', async function () {
-  await addToShoppingList();
-});
