7f91273ec3389ef98baca1ca8a58ef497616be26
YuliaKravchukS <ljuljek554@gmail.com>
2024-03-03 13:01:28 +0200

    add mob menu auth user

--- src/js/form-authorization.js
+++ src/js/form-authorization.js
@@ -1,17 +1,18 @@
 const firebaseApp = firebase.initializeApp({ 
-  apiKey: "AIzaSyDHiSfvnjzVWJqWI_IEEi6bW_1h4HyJ_fU",
+    apiKey: "AIzaSyDHiSfvnjzVWJqWI_IEEi6bW_1h4HyJ_fU",
   authDomain: "form-authorization-4890a.firebaseapp.com",
   projectId: "form-authorization-4890a",
   storageBucket: "form-authorization-4890a.appspot.com",
   messagingSenderId: "168346739495",
-  appId: "1:168346739495:web:c2181a801d16964f053f02"
+  appId: "1:168346739495:web:c2181a801d16964f053f02",
  });
    const db = firebaseApp.firestore();
    const auth = firebaseApp.auth()
 
 
 const btnSingUp = document.querySelector('.h-user')
-const btnSingUpMob = document.querySelector('.h-user-mobail')
+const btnSingUpMob = document.querySelector('.h-user-mobail-sign')
+const btnLogOutMob = document.querySelector('.h-user-mobail')
 const dialog = document.querySelector('#favDialog')
 const btnClose = document.querySelector('#btn-close')
 const btnOpenUp = document.querySelector('#btn-open-up')
@@ -19,7 +20,8 @@ const btnOpenIn = document.querySelector('#btn-open-in')
 const linkSingUp = document.querySelector('.link-singUp')
 const linkSingIn = document.querySelector('.link-singIn')
 const wrapForm = document.querySelector('.wrap-form')
-
+const form = document.querySelector('.form-authorization')
+const navMob = document.querySelector('.user');
 
 
 btnClose.addEventListener("click", () => {
@@ -32,22 +34,28 @@ btnSingUpMob.addEventListener("click", () => {
     dialog.showModal();
   });
 
-  btnOpenUp.addEventListener("click", singUp)
-  function singUp(){
-    const email =document.getElementById('email').value;
-    const password =document.getElementById('current-password').value;
+btnOpenUp.addEventListener('click', singUp)
+form.addEventListener('submit', onFormSubmit)
+linkSingUp.addEventListener('click', onLinkSingUpClick)
+linkSingIn.addEventListener('click', onLinkSingInClick)
+//btnOpenIn.addEventListener('click', singIn)
+btnLogOutMob.addEventListener('click', onLogOutClick)
+
+
+function singUp() {
+  const email = document.getElementById('email').value;
+  const password =document.getElementById('current-password').value;
     firebase.auth().createUserWithEmailAndPassword(email, password)
   .then((result) => {
-   console.log(result);
+  console.log(result);
   })
   .catch((error) => {
     console.log(error.code); 
     console.log(error.message); 
   });
-  }
+}
 
-linkSingUp.addEventListener('click', onLinkSingUpClick)
-linkSingIn.addEventListener('click', onLinkSingInClick)
+
 
 function onLinkSingUpClick(){
   // btnOpenIn.classList.add('hidden');
@@ -63,36 +71,60 @@ function onLinkSingUpClick(){
   dialog.classList.remove('sizeIn');
   dialog.classList.add('sizeUp');
   wrapForm.innerHTML = markup;
+  const btnOpenUp = document.querySelector('#btn-open-up');
+  btnOpenUp.addEventListener('click', singUp);
 }
 
 function onLinkSingInClick(){
   const markup=`<div class="wrap-input">
   <input class="input" type="email" name="email" placeholder="EMAIL" id="email" autocomplete="email">
   <input class="input" type="password" name="password" placeholder="PASSWORD" id="current-password" autocomplete="current-password">
-</div>
+  </div>
   <button type="submit" id="btn-open-in">sing in</button>
   `;
   dialog.classList.remove('sizeUp');
   dialog.classList.add('sizeIn');
   wrapForm.innerHTML = markup;
+  const btnOpenIn = document.querySelector('#btn-open-in');
+  btnOpenIn.addEventListener('click', singIn);
 }
 
 
-btnOpenIn.addEventListener("click", singIn)
 
-function singIn(){ 
 
-  const email =document.getElementById('email').value;
-const password =document.getElementById('current-password').value;
+function singIn() {
+  const email = document.getElementById('email').value;
+  const password =document.getElementById('current-password').value;
   firebase.auth().signInWithEmailAndPassword(email, password)
   .then((result) => {
-   
     console.log(result, 1);
-   
   })
   .catch((error) => {
     console.log(error.code); 
     console.log(error.message); 
   });
 }
+btnOpenUp.addEventListener('click', singUp)
+form.addEventListener('submit', onFormSubmit)
+linkSingUp.addEventListener('click', onLinkSingUpClick)
+linkSingIn.addEventListener('click', onLinkSingInClick)
+//btnOpenIn.addEventListener('click', singIn)
 
+
+function onFormSubmit(e) {
+  e.preventDefault();
+  
+  navMob.classList.remove('hidden');
+  updateMenu(); 
+}
+
+function updateMenu() {
+  
+  btnSingUpMob.classList.add('hidden');
+  dialog.close();
+}
+
+function onLogOutClick() {
+  btnSingUpMob.classList.remove('hidden');
+  navMob.classList.add('hidden');
+}
