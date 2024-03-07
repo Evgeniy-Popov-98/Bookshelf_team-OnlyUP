c6a3053d00aec94ea53f44b28ad0a0b77aa98158
Evgeniy-Popov-98 <143257644+Evgeniy-Popov-98@users.noreply.github.com>
2024-03-07 16:56:35 +0200

    first-fix-mobailMenu

--- src/js/colorPage.js
+++ src/js/colorPage.js
@@ -1,15 +1,17 @@
 document.addEventListener('DOMContentLoaded', function () {
   const homeLink = document.querySelector('.js-hhome');
   const shoppingLink = document.querySelector('.js-hshopping');
+  const homeMobLink = document.querySelector('.hmodal-home-btn');
+  const shoppingMobLink = document.querySelector('.hmodal-shopping-link');
 
   if (
     window.location.pathname === '/index.html' ||
     window.location.pathname === '/'
   ) {
-    homeLink.classList.add('active');
-    shoppingLink.classList.remove('active');
+    homeLink.classList.toggle('active');
+    shoppingLink.classList.toggle('active');
   } else {
-    shoppingLink.classList.add('active');
-    homeLink.classList.remove('active');
+    // homeMobLink.classList.toggle('mob-nav-color');
+    // shoppingMobLink.classList.toggle('mob-nav-color');
   }
 });
