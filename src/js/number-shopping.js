7c621a8f53d5ec759c7bb84bb78193ac08820c02
Yevhenii <143257644+Evgeniy-Popov-98@users.noreply.github.com>
2024-03-09 17:56:38 +0200

    Update number-shopping.js

--- src/js/number-shopping.js
+++ src/js/number-shopping.js
@@ -6,7 +6,7 @@ document.addEventListener('DOMContentLoaded', chekInfoNumber);
 
 export default function chekInfoNumber() {
   let data = infoItemLocalStorage(TASKS_KEY);
-  if (data.length === 0) {
+  if (!data.length === 0) {
     numberInfo.innerHTML = `${data.length}`;
   } else {
     numberInfo.innerHTML = '0';
